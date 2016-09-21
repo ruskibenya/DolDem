84e80777081843dc0600d492409ca0c9aee49351
Benjamin Aronov <Ruskibear@Benjamins-MacBook-Air.local>
2016-09-21 14:09:11 -0500

    angularjs/module wrapping issue

--- app/DolDem.js
+++ app/DolDem.js
@@ -1,5 +1,3 @@
-
-
 /**
  * Makes an AJAX request to the /search endpoint of the API, using the
  * query string that was passed in
@@ -7,12 +5,14 @@
  * if successful, updates model.browseItems appropriately and then invokes
  * the callback function that was passed in
  */
-function searchUPC(query) {
+ function searchUPC(query, callback) {
+
+console.log("searchUPC was called");
 
     // api credentials
     var api_key = 'SEM3AFEFE0A3431261B870FB7DC3BDF7FED2';
     var api_secret = 'N2JjNjlmNzEwNDliOTM4N2YxMTI3NWEwYTJhMWI5MjE';
-    var sem3 = require('./../lib/main.js')(api_key,api_secret);
+    var sem3 = require('semantics3-node')(api_key,api_secret);
 
     //require(['semantics3'], function(sem3wall) {
       //var sem3 = sem3wall(api_key, api_secret)
--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
+
+  // delete if jquery script in index is necessary
+  require('jquery');
   console.log('Initialized app');
 });
