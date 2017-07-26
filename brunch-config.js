256a7e14ecc54b87c813fcb42f17d2f4dac0d492
ruskibenya <baronov@tulane.edu>
2017-07-26 15:45:59 -0500

    cleaned up brunch-config

--- brunch-config.js
+++ brunch-config.js
@@ -1,31 +1,18 @@
 module.exports = {
-  npm: {
-        globals: {
-            $: 'jquery',
-  }},
-
-  server:{run: true},
-
   files: {
-      javascripts: {
-        joinTo: {
+    javascripts: {
+      joinTo: {
         'vendor.js': /^(?!app)/,
-        'app.js': /^app/,
+        'app.js': /^app/
       }
     },
     stylesheets: {
       joinTo: {
-              "app.css": [/^vendor/, /^bower_components/, /^app/]
-          },
-          order: {
-              after: [/^app/]
-          }
+        "app.css": [/^vendor/, /^bower_components/, /^app/]
+      },
+      order: {
+        after: [/^app/]
+      }
     }
-  },
-
-  plugins: {
-    babel: {presets: ['es2015']}
   }
-
-
 };
